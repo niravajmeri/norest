1c97e22924ba1205e94387bd39563eb42e36103e
niravajmeri <nirav.ajmeri@gmail.com>
2015-08-04 01:24:35 -0400

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Norest::Application.config.secret_key_base = '1856888f9a60ea44193160e22a37a2d6789df3022cf2a992e2be545e3ff5f0abf6b9b053efcff0ab0f99c7ee2d789bf3db21ec50e526b6c3e48f605657a1d71d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Norest::Application.config.session_store :cookie_store, key: '_norest_session'
